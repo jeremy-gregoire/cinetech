2019d819a3c13ee6f0a337b42261755e67c1d715
clarisse.lebaut <clarisse.le-baut@laplateforme.io>
2024-04-24 16:56:28 +0200

    up part of detail page - designed

--- details.js
+++ details.js
@@ -4,7 +4,7 @@ const options = {
   method: "GET", // méthode GET --> recupérer sur le serveur
   headers: {
     accept: "application/json", // dire que je prend du JSON
-    Authorization: "", // permet de d'authentifier notre demande
+    Authorization: "Bearer " + keyAPI, // permet de d'authentifier notre demande
   },
 };
 
@@ -14,8 +14,40 @@ const id = params.get("id"); // on définit id --> on le récupère tel quel dan
 console.log(`L'ID dans l'URL est :`, id); // cet id permet de mettre à jour en fonction de l'ID présent dans l'URL
 
 // ----------------------------------------------------------
+const container = document.getElementById("container");
+const bandeau = document.getElementById("bandeau");
+const affiche = document.getElementById("poster");
+const info = document.getElementById("small_info");
+info.classList.add();
+const language = document.getElementById("language_info");
+language.classList.add("d-flex");
+const pitch = document.getElementById("pitch");
+const statistic = document.getElementById("statistic");
+statistic.classList.add("text-center");
+const companies = document.getElementById("companies");
+const cast = document.getElementById("cast");
+const suggestions = document.getElementById("suggestions");
+
+// --------------------------------------
+// --------------------------------------
+const details = document.getElementById("details");
+// --------------------------------------
+// --------------------------------------
 
 // cette fonction permet de récupérer toutes les informations du films et des les imprimer dans la console
+async function fetchPicture() {
+  const response = await fetch(`https://api.themoviedb.org/3/movie/${id}/images?api_key=${keyAPI}`);
+  const data = await response.json();
+  console.log("les images du film sont :", data);
+
+  const hero = document.createElement("img");
+  hero.src = "https://image.tmdb.org/t/p/w500" + data.backdrops[0].file_path;
+  hero.classList.add("fullscreen-img");
+  hero.style.opacity = "0.50";
+  bandeau.appendChild(hero);
+}
+fetchPicture();
+
 async function fetchData() {
   const response = await fetch(
     `https://api.themoviedb.org/3/movie/${id.toString()}?api_key=${keyAPI}` //toString change l'apparence dans de l'impression dans la console
@@ -26,29 +58,72 @@ async function fetchData() {
   console.log(`Les infos du film ${id} sont : `, data); // --> permet de farie apparaitre les infos dans la même ligne comme A. m'a montré
 
   //crée les éléments dans la page web
+  const poster = document.createElement("img");
+  poster.src = "https://image.tmdb.org/t/p/w400" + data.poster_path; //pour récupérer l'image correspondant au film
+  poster.classList.add("bandeau", "rounded", "shadow-lg", "border", "border-light");
+  affiche.appendChild(poster);
+
   const title = document.createElement("h1");
   title.innerText = data.title;
-  document.body.appendChild(title);
-
-  const original_language = document.createElement("p");
-  original_language.innerText = data.original_language;
-  document.body.appendChild(original_language);
-
-  const origin_country = document.createElement("p");
-  origin_country.innerText = data.origin_country;
-  document.body.appendChild(origin_country);
+  info.appendChild(title);
 
   const release_date = document.createElement("p");
-  release_date.innerText = data.release_date;
-  document.body.appendChild(release_date);
+  release_date.innerText = "Réalisé le : " + data.release_date;
+  info.appendChild(release_date);
+
+  const runtime = document.createElement("p");
+  runtime.innerText = "Durée : " + data.runtime;
+  info.appendChild(runtime);
+
+  // Créer un tableau pour stocker les noms des genres
+  const genreNames = [];
+  // Boucle à travers chaque élément du tableau genres
+  data.genres.forEach(function (genreItem) {
+    // Ajouter le nom du genre actuel au tableau
+    genreNames.push(genreItem.name);
+  });
+  // Créer un paragraphe pour afficher les noms des genres
+  const genreParagraph = document.createElement("p");
+  // Définir le texte du paragraphe en joignant les noms des genres avec des virgules
+  genreParagraph.innerText = "Genre: " + genreNames.join(", ");
+  // Ajouter le paragraphe à l'élément parent (info dans votre cas)
+  info.appendChild(genreParagraph);
+
+  const original_language = document.createElement("p");
+  original_language.innerText = "Langue originla : " + data.original_language;
+  original_language.classList.add("me-5");
+  language.appendChild(original_language);
+
+  const origin_country = document.createElement("p");
+  origin_country.innerText = "Pays d'origine : " + data.origin_country;
+  origin_country.classList.add("me-5");
+  language.appendChild(origin_country);
+
+  // Créer un tableau pour stocker les noms des genres
+  const product_country = [];
+  // Boucle à travers chaque élément du tableau genres
+  data.production_countries.forEach(function (producountryItem) {
+    // Ajouter le nom du genre actuel au tableau
+    product_country.push(producountryItem.name);
+  });
+  // Créer un paragraphe pour afficher les noms des genres
+  const production_country = document.createElement("p");
+  // Définir le texte du paragraphe en joignant les noms des genres avec des virgules
+  production_country.innerText = "Pays de production: " + product_country.join(", ");
+  // Ajouter le paragraphe à l'élément parent (info dans votre cas)
+  language.appendChild(production_country);
 
   const overview = document.createElement("p");
   overview.innerText = data.overview;
-  document.body.appendChild(overview);
+  pitch.appendChild(overview);
 
-  const poster = document.createElement("img");
-  poster.src = "https://image.tmdb.org/t/p/w500" + data.poster_path; //pour récupérer l'image correspondant au film
-  document.body.appendChild(poster);
+  const stats = document.createElement("p");
+  stats.innerText = `Popularité : ${data.popularity} Nombre de votes : ${data.vote_average} Moyenne des votes : ${data.vote_count} `;
+  statistic.appendChild(stats);
+
+  const budget = document.createElement('p');
+  budget.innerText = "Budget du film : " + data.budget;
+  companies.appendChild(budget)
 }
 fetchData();
 
@@ -77,7 +152,6 @@ async function fecthMovieSimilar() {
   const data = await response.json();
   console.log("les films similaire de " + id + " sont : ", data);
 }
-
 fecthMovieSimilar();
 
 // ----------------------------------
