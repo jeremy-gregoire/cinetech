d8d8588cef9f855dd859ddd60ea9b6b723aa557d
jeremy-gregoire <jeremy.gregoire@laplateforme.io>
2024-04-26 11:28:24 +0200

    Adding basic for favorites

--- favorites.js
+++ favorites.js
@@ -1,7 +1,8 @@
-const TOKEN =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNmZjY2QwMTIwZDk0MDhlMGI0NWU0ZTIzZGY4NDIyZiIsInN1YiI6IjY2Mjc1NjU3MjU4ODIzMDE3ZDkzYWUyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zML1TcSHnqMrf5F2yVrQyOsLmCmsbjncCBqk_36Fn9g';
+const TOKEN = '';
 
 const FAVORITES = [];
+
+function updateMovieListing() {}
 
 fetch('https://api.themoviedb.org/3/movie/popular', {
   method: 'GET',
@@ -13,11 +14,6 @@ fetch('https://api.themoviedb.org/3/movie/popular', {
   .then((response) => response.json())
   .then((data) => {
     const movies = data.results;
-    FAVORITES.push(movies[0]);
-    FAVORITES.push(movies[3]);
-    FAVORITES.push(movies[7]);
-    FAVORITES.push(movies[15]);
-    FAVORITES.push(movies[19]);
 
     movies.forEach((movie) => {
       const movieItem = document.createElement('div');
