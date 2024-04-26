84aea1baa437219a810bba2540bf579d72cb8421
clarisse.lebaut <clarisse.le-baut@laplateforme.io>
2024-04-26 09:34:56 +0200

    Adding the design of the header

--- header-scripts.js
+++ header-scripts.js
@@ -1,21 +1,21 @@
-const API_KEY = '';
+const API_KEY = "";
 const OPTIONS = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
+    accept: "application/json",
     Authorization: `Bearer ${API_KEY}`,
   },
 };
 
 export async function getMoviesAndSeriesAutocompletion() {
-  let urlMovies = ' https://api.themoviedb.org/3/movie/popular?language=fr-FR';
-  let urlSeries = ' https://api.themoviedb.org/3/tv/popular?language=fr-FR';
+  let urlMovies = " https://api.themoviedb.org/3/movie/popular?language=fr-FR";
+  let urlSeries = " https://api.themoviedb.org/3/tv/popular?language=fr-FR";
 
   try {
     let responses = await Promise.all([fetch(urlMovies, OPTIONS), fetch(urlSeries, OPTIONS)]);
 
     if (responses.every((response) => !response.ok && response.status !== 200)) {
-      console.error('Impossible to get movies and series!');
+      console.error("Impossible to get movies and series!");
       return;
     }
 
--- header.js
+++ header.js
@@ -1,12 +1,43 @@
-import { getMoviesAndSeriesAutocompletion } from './header-scripts.js';
+import { getMoviesAndSeriesAutocompletion } from "./header-scripts.js";
 
-const searchBar = document.querySelector('#searchBar');
-const btnSearch = document.querySelector('#btnSearch');
-const autoCompletionList = document.querySelector('#autoCompletionList');
+// création de la barre de navigation
+const nav = document.createElement("nav");
+nav.innerHTML = `
+    <div class="d-flex">
+        <a class="nav-item m-2 ms-5 hover-style" href="../testrecupdata.html">HOME</a>
+        <a class="nav-item m-2 hover-style" href="../pages/films.html">FILMS</a>
+        <a class="nav-item m-2 hover-style" href="../pages/series.html">SERIES</a> 
+        <a class="nav-item m-2 hover-style" href="../pages/favoris.html">FAVORIS</a>
+    </div>
+    <div class="m-2 me-5">
+        <div class="input-group">
+            <input type="text" id="searchBar" list="autoCompletionList" placeholder="Rechercher" />
+            <button type="button" id="btnSearch">O</button>
+            <datalist id="autoCompletionList"></datalist>
+        </div>
+    </div>
+`;
+//stylisation rapide
+nav.classList.add(
+  "navbar",
+  "bg-color",
+  "sticky-top",
+  "justify-content-between",
+  "align-item-center"
+);
+document.body.appendChild(nav);
+
+// Insérer la barre de navigation juste avant le premier élément du corps de la page
+const firstElement = document.body.firstChild;
+document.body.insertBefore(nav, firstElement);
+
+const searchBar = document.querySelector("#searchBar");
+const btnSearch = document.querySelector("#btnSearch");
+const autoCompletionList = document.querySelector("#autoCompletionList");
 
 // Redirect to the details page with two parameters: id and type
 const goToDetails = () => {
-  let options = document.querySelectorAll('option[request-info]');
+  let options = document.querySelectorAll("option[request-info]");
   let selected = null;
 
   for (let i = 0; i < options.length; i++) {
@@ -18,20 +49,20 @@ const goToDetails = () => {
   }
 
   if (selected) {
-    let valueParams = selected.getAttribute('request-info').split(':');
+    let valueParams = selected.getAttribute("request-info").split(":");
     window.location.href = `${window.location.origin}/details.html?id=${valueParams[1]}&type=${valueParams[0]}`;
   } else {
-    console.error('Your search is empty or not found!');
+    console.error("Your search is empty or not found!");
     return;
   }
 };
 
 // By default the search is empty
-searchBar.value = '';
+searchBar.value = "";
 
 // Go to the details, when the enter key is pressed
-searchBar.addEventListener('keydown', (e) => {
-  if (e.key !== 'Enter') {
+searchBar.addEventListener("keydown", (e) => {
+  if (e.key !== "Enter") {
     return;
   }
 
@@ -39,17 +70,17 @@ searchBar.addEventListener('keydown', (e) => {
 });
 
 // Go to the details, when the button is pressed
-btnSearch.addEventListener('click', () => {
+btnSearch.addEventListener("click", () => {
   goToDetails();
 });
 
 // Generates auto complition items
 getMoviesAndSeriesAutocompletion().then((data) => {
   data.forEach((content) => {
-    let type = content.release_date ? 'movie' : 'tv';
+    let type = content.release_date ? "movie" : "tv";
 
-    const option = document.createElement('option');
-    option.setAttribute('request-info', `${type}:${content.id}`);
+    const option = document.createElement("option");
+    option.setAttribute("request-info", `${type}:${content.id}`);
     option.value = content.title || content.name;
     autoCompletionList.appendChild(option);
   });
